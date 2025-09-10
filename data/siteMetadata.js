5c1f0b83fb43d5624d59acc031c2b7ee6deddac2
Pat Nadolny <patnadolny@gmail.com>
2025-09-10 17:56:35 -0400

    fix beehiiv spelling in comment

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -50,7 +50,7 @@ const siteMetadata = {
     // },
   },
   newsletter: {
-    // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus, beehive
+    // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus, beehiiv
     // Please add your .env file and modify it according to your selection
     provider: 'buttondown',
   },
